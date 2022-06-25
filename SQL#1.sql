c7a453089cec400a9d8b62bb7293993357a0b88e
DaElias <58747265+DaElias@users.noreply.github.com>
2022-06-25 18:40:59 -0500

    add first commit

---
+++ SQL#1.sql
@@ -0,0 +1,20 @@
+-- ##LLAMAR DATOS
+-- SELECT * FROM database.people where city = "México"; 
+-- SELECT * FROM database.people LIMIT 0, 10;
+
+-- ## ISERTAR DATOS EN TABLA X 
+-- INSERT INTO `database`.`peoples` (`idpeoples`, `last_name`, `first_name`, `address`, `city`) 
+-- VALUES ('1', 'Vásquez', 'Israel', 'Calle Famosa Num 1', 'México'),
+-- 	       ('2', 'Hernández', 'Mónica', 'Reforma 222', 'México'),
+-- 	       ('3', 'Alanis', 'Edgar', 'Central 1', 'Monterrey');
+           
+-- ## ELIMINAR UNA COLUMNA
+-- ALTER TABLE `database`.`people` 
+-- DROP COLUMN `date_of_birth`;
+ 
+
+
+
+
+
+
