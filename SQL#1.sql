877505f87d4b3d97810f095bf6c8843ef4675240
DaElias <58747265+DaElias@users.noreply.github.com>
2022-06-25 20:04:34 -0500

    more notes added

---
+++ SQL#1.sql
@@ -0,0 +1,26 @@
+-- ##LLAMAR DATOS
+-- SELECT * FROM database.people where city = "México"; 
+-- SELECT * FROM database.people LIMIT 0, 10;
+
+-- ## ISERTAR DATOS EN TABLA X 
+-- INSERT INTO `database`.`peoples` (`idpeoples`, `last_name`, `first_name`, `address`, `city`) 
+-- VALUES ('1', 'Vásquez', 'Israel', 'Calle Famosa Num 1', 'México'),
+-- 	       ('2', 'Hernández', 'Mónica', 'Reforma 222', 'México'),
+-- 	       ('3', 'Alanis', 'Edgar', 'Central 1', 'Monterrey');
+           
+-- ## ELIMINAR UNA COLUMNA
+-- ALTER TABLE `database`.`people` 
+-- DROP COLUMN `date_of_birth`;
+ 
+
+-- ##UPDATE
+-- update people set first_name="ISRAEL" where idpeoples=1; 
+
+-- ##DELETE
+-- delete from people where idpeoples=3; 
+
+
+-- ##SELECT
+-- select last_name from people; 
+
+
